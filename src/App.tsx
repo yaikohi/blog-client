a2b5be37ce88042720b789c5fe03f79bcea20fd0
yaikohi <erik.beem@hotmail.com>
2024-01-13 19:48:58 +0100

    good enough

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,43 @@
+import { createResource, For } from "solid-js";
 import "./App.css";
+import { CommentForm, PostForm } from "./forms";
 // https://github.com/fabian-hiller/modular-forms/issues/157
-import { createForm, SubmitHandler } from "@modular-forms/solid";
+async function fetchPosts() {
+  const url = `http://localhost:4002/posts`;
+  const resp = await fetch(url, { method: "GET" });
+  const res = await resp.json();
 
-type PostFormType = {
-  title: string;
-  content: string;
-};
-type CommentFormType = {
-  content: string;
-};
+  return res;
+}
 
 function App() {
-  const URL_POSTS = `http://localhost:4000`;
-  const URL_COMMENTS = `http://localhost:4001`;
-  const URL_QS = `http://localhost:4002`;
+  const [posts] = createResource(fetchPosts);
 
-  const [postFormStore, Post] = createForm<PostFormType>();
-  const [commentFormStore, Comment] = createForm<CommentFormType>();
-  const handlePostSubmit: SubmitHandler<PostFormType> = async (values) => {
-    await fetch(`${URL_POSTS}/posts`, {
-      method: "POST",
-      body: JSON.stringify({ content: values.content, title: values.title }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  };
-
-  const handleCommentSubmit: SubmitHandler<CommentFormType> = async (
-    values,
-  ) => {
-    const id = 1;
-    await fetch(`${URL_COMMENTS}/posts/${id}/comments`, {
-      method: "POST",
-      body: JSON.stringify({ content: values.content }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  };
   return (
     <>
-      <div class="flex mx-auto max-w-[600px] justify-center">
-        <div class="">
-          <h2>Create new post</h2>
-          {/* POST FORM*/}
-          <Post.Form onSubmit={handlePostSubmit}>
-            <Post.Field name="title">
-              {(field, props) => <input {...props} type="text" />}
-            </Post.Field>
-            <Post.Field name="content">
-              {(field, props) => <input {...props} type="text" />}
-            </Post.Field>
-            <button>Submit</button>
-          </Post.Form>
-        </div>
+      <div class="flex-col mx-auto max-w-[600px] justify-center">
+        <h1>Blog!</h1>
+        <h2>Posts</h2>
+        <PostForm />
         <div>
-          <h2>Create a new comment</h2>
-          {/* COMMENT FORM*/}
-          <Comment.Form onSubmit={handleCommentSubmit}>
-            <Comment.Field name="content">
-              {(field, props) => <input {...props} type="text" />}
-            </Comment.Field>
-            <button class="">Submit</button>
-          </Comment.Form>
+          {/* POSTS */}
+          {posts.loading && <p>Posts are loading...</p>}
+          {posts.error && <p>Error...</p>}
+          {posts() && (
+            <>
+              <For each={posts()}>
+                {(post) => (
+                  <>
+                    <div class="p-4 border-2 border-green-700 rounded-xl my-8">
+                      <h3>{post.title}</h3>
+                      <p>{post.content}</p>
+                      <CommentForm postId={post.id} />
+                    </div>
+                  </>
+                )}
+              </For>
+            </>
+          )}
         </div>
       </div>
     </>
---
+++ src/forms.tsx
@@ -0,0 +1,76 @@
+import { createForm, SubmitHandler } from "@modular-forms/solid";
+
+type PostFormType = {
+  title: string;
+  content: string;
+};
+type CommentFormType = {
+  content: string;
+};
+export function PostForm() {
+  const URL_POSTS = `http://localhost:4000`;
+  const [postFormStore, Post] = createForm<PostFormType>();
+
+  const handlePostSubmit: SubmitHandler<PostFormType> = async (values) => {
+    await fetch(`${URL_POSTS}/posts`, {
+      method: "POST",
+      body: JSON.stringify({ content: values.content, title: values.title }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+  return (
+    <div class="">
+      <h2>Create new post</h2>
+      {/* POST FORM*/}
+      <Post.Form onSubmit={handlePostSubmit} class="flex-col gap-2 my-8">
+        <div class="my-2">
+          <Post.Field name="title">
+            {(field, props) => <input {...props} type="text" />}
+          </Post.Field>
+        </div>
+        <div class="my-2">
+          <Post.Field name="content">
+            {(field, props) => <input {...props} type="text" />}
+          </Post.Field>
+        </div>
+        <button>Submit</button>
+      </Post.Form>
+    </div>
+  );
+}
+export function CommentForm({ postId }: { postId: string }) {
+  const URL_COMMENTS = `http://localhost:4001`;
+  const [commentFormStore, Comment] = createForm<CommentFormType>();
+  const handleCommentSubmit: SubmitHandler<CommentFormType> = async (
+    values,
+  ) => {
+    await fetch(`${URL_COMMENTS}/posts/${postId}/comments`, {
+      method: "POST",
+      body: JSON.stringify({ content: values.content }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+  return (
+    <>
+      <div>
+        <h4>Create a new comment</h4>
+        {/* COMMENT FORM*/}
+        <Comment.Form
+          onSubmit={handleCommentSubmit}
+          class="flex-col my-8 gap-2"
+        >
+          <div class="my-2">
+            <Comment.Field name="content">
+              {(field, props) => <input {...props} type="text" />}
+            </Comment.Field>
+          </div>
+          <button class="">Submit</button>
+        </Comment.Form>
+      </div>
+    </>
+  );
+}
